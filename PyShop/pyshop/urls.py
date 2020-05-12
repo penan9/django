042e309576172ef3faeaed27246568431ce4e925
penan9 <58354492+penan9@users.noreply.github.com>
2020-05-13 02:58:23 +0800

    ok2

--- PyShop/pyshop/urls.py
+++ PyShop/pyshop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
     path('products/', include('products.urls')),
     path('products/new', include('products.urls')),
     path('products/sales', include('products.urls')),
